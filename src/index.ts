9cf05fd2be406b0ca170fee36959c22d1453badb
techa <techa27@gmail.com>
2025-01-29 11:24:12 +0900

    refactor: clean index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,6 @@
-import { View } from './View.js'
 import { mount } from 'svelte'
 import App from './components/App.svelte'
 
 mount(App, {
 	target: document.getElementById('app')!,
 })
-
-
-// document.addEventListener('DOMContentLoaded', () => {
-// 	const view = new View()
-
-// 	view.addEventToStart()
-// })
